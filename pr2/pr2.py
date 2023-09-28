1a7d2ad1987d6a5a6b70f620f9e9c0e3496c4103
cyberkylich <107815101+cyberkylich@users.noreply.github.com>
2023-09-28 12:47:57 +0300

    7 done

--- pr2/pr2.py
+++ pr2/pr2.py
@@ -1,14 +1,18 @@
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.graph_objs as go
+import plotly.express as px
+from sklearn.datasets import load_digits
+from sklearn.manifold import TSNE
+import umap
 
 
 def bar_grapf(df):
     fig = go.Figure()
-    fig.add_bar(x=[j for j in range(len(df))], y=df['subscribers'], marker=dict(color=[i for i in df['subscribers']],
-                                                                                colorscale='Inferno',
-                                                                                line=dict(color='black', width=2)))
+    fig.add_bar(x=[j for j in range(len(df))], y=df['subscribers'],
+                marker=dict(color=[i for i in df['subscribers']],
+                            colorscale='Inferno',
+                            line=dict(color='black', width=2)))
     fig.update_layout(title={'text': "Best youtubers statistic",
                              'y': 0.9,
                              'x': 0.5,
@@ -26,22 +30,11 @@ def bar_grapf(df):
 
 def circle_grapf(df):
     fig = go.Figure()
-    # low = []
-    # mid = []
-    # high = []
-    # for items in df["subscribers"]:
-    #     if items < 35_000_000:
-    #         low.append(items)
-    #     elif items < 80_000_000:
-    #         mid.append(items)
-    #     else:
-    #         high.append(items)
-
     low = 0
     mid = 0
     high = 0
     new_arr = []
-    labels = ['subscribers < 35 000 000', 'subscribers < 80 000 000', 'subscribers > 80 000 000']
+    labels = ['subscribers < 35kk', 'subscribers < 80kk', 'subscribers > 80kk']
     for items in df["subscribers"]:
         if items < 35_000_000:
             low += 1
@@ -58,18 +51,74 @@ def circle_grapf(df):
     return
 
 
+def line_grapf(df):
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5))
+    ax1.plot([j for j in range(len(df))], df['highest_yearly_earnings'], marker='o',
+             color='crimson', markerfacecolor='white', markeredgecolor='black', markersize=2)
+    ax1.set_xlabel('Rank')
+    ax1.set_ylabel('Highest yearly earnings')
+    ax1.grid(True, color='mistyrose', linewidth=2)
+    ax2.plot([j for j in range(len(df))], df['subscribers'], marker='o',
+             color='crimson', markerfacecolor='white', markeredgecolor='black', markersize=2)
+    ax2.set_xlabel('Rank')
+    ax2.set_ylabel('Subscribers')
+    ax2.grid(True, color='mistyrose', linewidth=2)
+    ax3.plot([j for j in range(len(df))], df['video views'], marker='o',
+             color='crimson', markerfacecolor='white', markeredgecolor='black', markersize=2)
+    ax3.set_xlabel('Rank')
+    ax3.set_ylabel('Video views')
+    ax3.grid(True, color='mistyrose', linewidth=2)
+    plt.show()
+    return
+
+
+def tsne_grapf():
+    X, y = load_digits(return_X_y=True)
+    # digits = load_digits()
+    # fig, axs = plt.subplots(2, 5, sharey=False, tight_layout=True, figsize=(12, 6), facecolor='white')
+    # n = 0
+    # plt.gray()
+    # for i in range(0, 2):
+    #     for j in range(0, 5):
+    #         axs[i, j].matshow(digits.images[n])
+    #         axs[i, j].set(title=y[n])
+    #         n = n + 1
+    # plt.show()
+    embed = TSNE(n_components=2, perplexity=10, random_state = 123)
+    X_embedded = embed.fit_transform(X)
+    plt.scatter(X_embedded[:, 0], X_embedded[:, 1], c=y, s=0.5)
+    plt.show()
+    return
+
+
+def umap_grapf():
+    X, y = load_digits(return_X_y=True)
+    manifold = umap.UMAP(n_neighbors=50, min_dist=0.6, random_state=123).fit(X, y)
+    X_reduced = manifold.transform(X)
+    plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=y, s=0.5)
+    plt.show()
+    return
+
+
 def main():
     df_orig = pd.read_csv("youtube_stat.csv", encoding_errors="replace")
     df = df_orig.drop_duplicates(subset="subscribers")
-    print(len(df))
     print(df.info())
     print(df.head())
-    a = input("Какую диаграмму вывести?(1-столбчатая, 2-круговая, 3-линейная): ")
+    a = input("Какую диаграмму вывести?(1-столбчатая, 2-круговая, 3-линейная, 4-t-SNE, 5-UMAP): ")
     a = int(a)
     if a == 1:
         bar_grapf(df)
     elif a == 2:
         circle_grapf(df)
+    elif a == 3:
+        line_grapf(df)
+    elif a == 4:
+        tsne_grapf()
+    elif a == 5:
+        umap_grapf()
+    else:
+        print("Неправильное значение")
     return
 
 
